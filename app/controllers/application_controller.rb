d2baab908a183dc4e8eef48460cb46b04fa64333
Mana <sabrina.mardjoeki@gmail.com>
2018-05-15 21:56:45 +0200

    404 page init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :danger
-  def not_found
-   raise ActionController::RoutingError.new('Not Found')
- rescue
-   render_404
-  end
-
- def render_404
-   render file: "#{Rails.root}/public/404", status: :not_found
- end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   root to: 'welcome#index'
   get '/prices', to: 'pages#prices', as: 'prices'
   get '/join', to: 'pages#join', as: 'join'
   get '/contact', to: 'pages#contact', as: 'contact'
   get '/:token/confirm_email/', :to => "requests#confirm_email", as: 'confirm_email'
   get '/:token/confirm_raise/', :to => "requests#confirm_raise", as: 'confirm_raise'
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   resources :requests, only: [:new, :create]
 
