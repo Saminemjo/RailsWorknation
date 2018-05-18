2e7b323cae6b16aac99f5d017323ace18a33acfc
Mana <sabrina.mardjoeki@gmail.com>
2018-05-18 20:42:01 +0200

    good fix

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -26,30 +26,25 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.new(request_params)
-    if @request.valid?
       if @request.save
-        @request.send :set_confirmation_token
-        @request.raised_at = @request.created_at
-        @request.save(validate: false)
         # Tell the ConfirmMailer to send a welcome email after save
         ConfirmMailer.with(request: @request).registration_confirmation.deliver_now
         redirect_to root_path, success: 'A confirmation email has been sent, please check your mailbox to complete your subscription on the waiting list.'
-      end
-    else
+      else
        render 'new'
-    end
+      end
   end
 
-  def destroy
-    @request = Request.find(params[:id])
-    @request.destroy
-    redirect_to root_path, success: 'Request deleted'
-  end
+  # def destroy
+  #   @request = Request.find(params[:id])
+  #   @request.destroy
+  #   redirect_to root_path, success: 'Request deleted'
+  # end
 
   def confirm_email
     request = Request.find_by_confirm_token(params[:token])
      if request
-       request.send :validate_email
+       request.validate_email
        request.save
        redirect_to root_path, success: 'Your email has been confirmed, you are registered to the waiting List. See you soon !'
      else
@@ -60,7 +55,7 @@ class RequestsController < ApplicationController
   def confirm_raise
     request = Request.find_by_confirm_token(params[:token])
      if request
-       request.send :validate_raise
+       request.validate_raise
        request.save
        redirect_to root_path, success: 'Your request has been confirmed, you are still registered to the waiting List. See you soon !'
      else
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,30 +1,31 @@
 class Request < ApplicationRecord
-  validates :name, presence: true, on: :create
-  validates :email, presence: true, on: :create
-  validates :biography, presence: true, on: :create
-  validates :email, uniqueness: true, email: true, on: :create
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :biography, presence: true
+  validates :email, uniqueness: true, email: true
   validates :phone, format: { with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message: ' Number must have a length of minimum 7 and maximum 13 numbers' }, on: :create
 
   default_scope { order(created_at: :asc) }
-  scope :accepted, -> { where(accepted: 1) }
-  scope :confirmed, -> { where(email_confirmed: 1) }
-  scope :unconfirmed, -> { where(email_confirmed: 0) }
-  scope :expired, -> { where(expired: 1) }
+  scope :accepted, -> { where(accepted: true) }
+  scope :confirmed, -> { where(email_confirmed: true) }
+  scope :unconfirmed, -> { where(email_confirmed: false) }
+  scope :expired, -> { where(expired: true) }
 
-  def self.accept!
-    request = Request.where(accepted: 0).where(expired: 0).where(email_confirmed: 1).first
-    request.update(accepted: 1)
+  before_save do
+    self.set_confirmation_token
+    self.raised_at = Time.now
   end
 
-  private
+  def self.accept!
+    request = Request.where(accepted: false).where(expired: false).where(email_confirmed: true).first
+    request.update(accepted: true)
+  end
 
   def set_confirmation_token
     if confirm_token.blank?
       self.confirm_token = SecureRandom.urlsafe_base64.to_s
     end
   end
-
-  private
 
   def validate_raise
     self.raised_at = Time.now
@@ -37,20 +38,19 @@ class Request < ApplicationRecord
   end
 
   def self.registration_raise
-    request = Request.where(email_confirmed: 1).where(accepted: 0).where(expired: 0).all
+    request = Request.where(email_confirmed: true).where(accepted: false).where(expired: false).all
     @result = request.where('raised_at BETWEEN ? AND ?', 91.days.ago.beginning_of_day, 91.days.ago.end_of_day)
     @result.each do |r|
-      r.send :set_confirmation_token
+      r.set_confirmation_token
       r.save(validate: false)
       index = @result.index(r) + 1
       ConfirmMailer.registration_raise(r, index).deliver
-      
     end
   end
 
   def self.set_to_expired
-    withoutConfirm = Request.where(email_confirmed: 0).where('created_at < ?', 8.days.ago)
-    withoutRaise = Request.where(expired: 0).where(accepted: 0).where('raised_at < ?', 99.days.ago)
+    withoutConfirm = Request.where(email_confirmed: false).where('created_at < ?', 8.days.ago)
+    withoutRaise = Request.where(expired: false).where(accepted: false).where('raised_at < ?', 99.days.ago)
     result = withoutConfirm + withoutRaise
     result.each do |r|
       r.expired = true
